dba8540c29912901c9339e9e47a14bd2f846cbc2
Bartholomeus Mortimis <140279247+Bartholomeus-Mortimis@users.noreply.github.com>
2025-08-06 12:59:50 -0400

    v1.1
    - Removed CustomComponentBehaviour resource, moved custom functions into GunComponent script. Instead of using CustomComponentBehaviour, GunComponent is now easily extendable for custom code.
    
    - Added built-in engine documentation
    
    - Updated example scene to Godot 4.4

--- Example Project/Example Project/example_scene/example_scene.gd
+++ Example Project/Example Project/example_scene/example_scene.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@onready var character_body_2d: CharacterBody2D = $CharacterBody2D
+@onready var gun_component: Node2D = $CharacterBody2D/GunComponent
+
 func _process(_delta):
-	$CharacterBody2D/GunComponent.muzzle_position = $CharacterBody2D.position
-	$CharacterBody2D.look_at(get_global_mouse_position())
+	gun_component.muzzle_position = character_body_2d.position
+	character_body_2d.look_at(get_global_mouse_position())
 	if Input.is_action_pressed("shoot"):
-		$CharacterBody2D/GunComponent.shoot_gun()
+		gun_component.shoot_gun()
 	if Input.is_action_just_pressed("reload"):
-		$CharacterBody2D/GunComponent.reload_gun()
-	$CharacterBody2D.move_and_slide()
+		gun_component.reload_gun()
+	character_body_2d.move_and_slide()
--- Example Project/Godot-Gun-Manager-2D/README.md
+++ Example Project/Godot-Gun-Manager-2D/README.md
@@ -1,3 +1,4 @@
-Godot Gun Manager:
+Godot Gun Manager 2D
+v1.1
 
 Made by Bartholomeus Mortimis
